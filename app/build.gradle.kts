8250bd6dec5d8dbbe9c4856a6b17153538bcd7aa
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-05-28 17:30:45 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
