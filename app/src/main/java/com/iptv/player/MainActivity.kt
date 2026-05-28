7d56dacf480beb4c4b8c035d50e48f49bd13d87a
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-05-28 16:23:01 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/iptv/player/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.iptv.player
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, PlayerActivity::class.java))
+        finish()
+    }
+}
