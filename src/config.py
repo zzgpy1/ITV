3bbbe296c71eecdc3ee6debd872dc0ff7a20bd91
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-06-14 16:48:42 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -39,9 +39,10 @@ def get_cdn_proxy() -> str:
 RAW_SOURCES = [
     "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/cn.m3u",
     "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv4.txt",
+    "https://raw.githubusercontent.com/zzgpy1/ipv6-iptv/master/tv/iptv4.txt",
     "https://raw.githubusercontent.com/zzgpy1/iptv-api/master/output/result.txt",
     "https://raw.githubusercontent.com/dogwalkerg/IPTV-collect-tv-txt/main/live.txt",
-    "https://raw.githubusercontent.com/zzgpy1/Collect-IPTV/main/best_sorted.m3u",
+    "https://raw.githubusercontent.com/zilong7728/Collect-IPTV/main/best_sorted.m3u",
     "https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u",
     "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u",
     "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u",
