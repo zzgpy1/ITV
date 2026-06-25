64688376cf99337faacd9b4ce9317dd8727eb9d4
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-06-25 16:16:52 +0800

    Delete src/gui directory

--- src/gui/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/gui/__init__.py
-"""GUI 模块"""
-
-from src.gui.main_window import IPTVMainWindow
-from src.gui.widgets import LogTextEdit, ChannelTable, DashboardWidget
-
-__all__ = ["IPTVMainWindow", "LogTextEdit", "ChannelTable", "DashboardWidget"]
