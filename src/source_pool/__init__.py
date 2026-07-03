cdc279e30abf0851d7f6d719e4cfd33b94c6a523
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-03 15:11:34 +0800

    Delete src/source_pool directory

--- src/source_pool/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/source_pool/__init__.py
-"""源池模块 - 新源发现与管理"""
-
-from src.source_pool.discoverer import SourceDiscoverer
-from src.source_pool.models import RawSource, SourceStatus
-
-__all__ = ["SourceDiscoverer", "RawSource", "SourceStatus"]
