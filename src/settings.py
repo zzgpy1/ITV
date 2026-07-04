9f153aaa39358e25d8c3d9136cb8849d951eba2f
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-04 23:36:49 +0800

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -74,6 +74,7 @@ class Settings:
         "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv4.txt",
         "https://raw.githubusercontent.com/zzgpy1/iptv-api/master/output/result.txt",
         "https://raw.githubusercontent.com/zzgpy1/Collect-IPTV/main/best_sorted.m3u",
+        "https://raw.githubusercontent.com/zzgpy1/ipv6-iptv/master/tv/iptv4.txt",
        ])
     DIRECT_SOURCES: List[str] = field(default_factory=lambda: [
         "https://tv.19860519.xyz/abc123",
