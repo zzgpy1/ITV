d26b4505592452ac04fd4f78010a7a64c836598f
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-22 18:42:47 +0800

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,8 @@
 # config/settings.py
 from pathlib import Path
 from typing import List, Optional
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # 路径
