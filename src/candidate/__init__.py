5cc817554351db47d46d13288ac7c1ac2d82755f
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-22 14:13:45 +0800

    Delete src/candidate directory

--- src/candidate/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/candidate/__init__.py
-"""候选版模块 - 新源观察验证"""
-
-from src.candidate.observer import CandidateObserver, ObservationResult
-from src.candidate.models import CandidateStatus
-
-__all__ = ["CandidateObserver", "ObservationResult", "CandidateStatus"]
