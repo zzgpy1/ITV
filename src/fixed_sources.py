8546f7e90c7069fac215ea1711e29d22db0f2659
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-22 14:30:01 +0800

    Create fixed_sources.py

---
+++ src/fixed_sources.py
@@ -0,0 +1,9 @@
+CCTV_FIXED_SOURCES = {
+    "CCTV-1": ["http://45.192.97.170:8880/play/1.m3u8"],
+    "CCTV-2": ["http://45.192.97.170:8880/play/2.m3u8"],
+    "CCTV-3": ["http://69.30.245.50/live/cctv3.m3u8"],
+    "CCTV-4": ["http://69.30.245.50/live/cctv4.m3u8"],
+    "CCTV-5": ["http://69.30.245.50/live/cctv5.m3u8"],
+    # ... 其余按需配置
+}
+ENABLE_FIXED_SOURCES = True
