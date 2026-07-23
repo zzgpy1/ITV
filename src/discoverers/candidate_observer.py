7a0260d8eb825e7240e494ae78c8919726449cc8
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-23 14:59:19 +0800

    Delete src directory

--- src/discoverers/candidate_observer.py
+++
@@ -1,8 +0,0 @@
-# src/observers/candidate_observer.py
-from src.repositories import repo_factory
-from src.logger import logger
-
-class CandidateObserver:
-    async def observe(self):
-        # 实际观察逻辑已由 speed_tester 和 orchestrator 处理
-        pass
